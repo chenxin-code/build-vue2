dafc0f98660a89b58998f5ddaaa67d6818f70f4c
chenxin <chenxin@gennlife.com>
2023-03-23 20:41:44 +0800

    build-vue2

---
+++ src/page/brother-component/brotherA.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h2>我喜欢吃{{ food }}</h2>
+    <select :value="food" @change="changeFood">
+      <option value="西瓜">西瓜</option>
+      <option value="黄瓜">黄瓜</option>
+      <option value="南瓜">南瓜</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import eventBus from '@/eventBus';
+
+export default {
+  data() {
+    return {
+      food: '西瓜',
+    };
+  },
+  methods: {
+    changeFood(e) {
+      this.food = e.target.value;
+      console.log('A $emit');
+      eventBus.$emit('Achange', e.target.value);
+    },
+  },
+  mounted() {
+    eventBus.$on('Bchange', (value) => {
+      console.log('A $on');
+      this.food = value;
+    });
+  },
+}
+</script>
+
+<style scoped>
+div {
+  margin: 5px;
+  border: 1px solid orangered;
+}
+</style>
--- src/page/brother-component/index.vue
+++ src/page/brother-component/index.vue
@@ -6,6 +6,11 @@
       <brother2 />
       <brother3 />
     </div>
+    <h1>兄弟组件eventBus通信</h1>
+    <div style="display: flex;">
+      <brotherA />
+      <brotherB />
+    </div>
   </div>
 </template>
 
@@ -13,11 +18,15 @@
 import brother1 from './brother1';
 import brother2 from './brother2';
 import brother3 from './brother3';
+import brotherA from './brotherA';
+import brotherB from './brotherB';
 export default {
   components:{
     brother1,
     brother2,
     brother3,
+    brotherA,
+    brotherB,
   }
 }
 </script>
