085152d579ccb489767173684256986f91cec6e7
chenxin <chenxin@gennlife.com>
2023-09-05 10:40:23 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,32 +29,26 @@ module.exports = {
 		'vue'
 	],
 	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
+		'indent': ['error', 'tab'],
+		'linebreak-style': ['error', 'windows'],
+		'quotes': ['error', 'single'],
+		'semi': ['error', 'always'],
 		// 关闭名称校验
 		'vue/multi-word-component-names': 'off',
 		// 指定数组的元素之间要以空格隔开(,后面)， never参数：[ 之前和 ] 之后不能带空格，always参数：[ 之前和 ] 之后必须带空格
-		'array-bracket-spacing': ['error', 'never'],
+		'array-bracket-spacing': ['warn', 'never'],
 		// 单行100个字符
-		//'max-len': ['error', { code : 100 }],
+		//'max-len': ['warn', { code : 100 }],
 		// 使用 === 替代 ==
-		'eqeqeq': ['error', 'allow-null'],
+		'eqeqeq': ['warn', 'allow-null'],
 		// 控制逗号前后的空格
-		'comma-spacing': ['error', { 'before': false, 'after': true }],
-		'no-unused-vars': 'off',
+		'comma-spacing': ['warn', { 'before': false, 'after': true }],
+		'no-unused-vars': 'warn',
+		// 禁止不必要的布尔转换
+		'no-extra-boolean-cast': 'warn',
+		// 禁止不必要的分号
+		'no-extra-semi': 'warn',
+		// 禁止在条件中使用常量表达式
+		'no-constant-condition': 'warn',
 	}
 };
